6cfd992a997015c80e43d591aee7a7313c2c152e
jmmarino723 <69175253+jmmarino723@users.noreply.github.com>
2020-08-27 22:11:11 -0500

    Change on email

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'juanmanuel723@gmail.com';
+  $receiving_email_address = '<juanmanuel723@gmail.com>';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -31,7 +31,7 @@
     'password' => '92112350324Aa',
     'port' => '587'
   );
-  */
+ */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
